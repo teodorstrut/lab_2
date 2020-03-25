5670ead902c3f64e83784b3ff7ad879e73105dc3
teodor_strut <teodorstrut@yahoo.com>
2020-03-25 16:48:58 +0200

    added new tests
    added new tests

--- src/test/java/vvss/AppTest.java
+++ src/test/java/vvss/AppTest.java
@@ -1,10 +1,13 @@
 package vvss;
 
 import domain.Student;
+import domain.Tema;
 import org.junit.Test;
 import repository.StudentRepository;
 import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
 import validation.StudentValidator;
+import validation.TemaValidator;
 import validation.Validator;
 
 import static org.junit.Assert.*;
@@ -37,5 +40,23 @@ public class AppTest
         StudentRepository repo = new StudentRepository(validator);
         Student s = repo.save(new Student("22","adrian",92));
         assertTrue(s == null);
+    }
+
+    @Test
+    public void Test3(){
+        Validator<Tema> validator = new TemaValidator();
+        TemaXMLRepository repository = new TemaXMLRepository(validator, "teme.xml");
+        Tema t = new Tema("12", "test_desc",12, 1);
+        repository.save(t);
+        assertNotNull(repository.findOne("12"));
+    }
+
+    @Test
+    public void Test4(){
+        Validator<Tema> validator = new TemaValidator();
+        TemaXMLRepository repository = new TemaXMLRepository(validator, "teme.xml");
+        Tema t = new Tema("999", "test_desc",24, 1);
+        repository.save(t);
+        assertNull(repository.findOne("999"));
     }
 }
